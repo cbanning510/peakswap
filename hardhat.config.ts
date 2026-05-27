373123101b48f6916989137e4b7154d9bed4cbd3
Chris Banning <chrisbanning510@gmail.com>
2026-05-26 20:45:37 -0400

    add Sepolia network config and dotenv support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { defineConfig } from "hardhat/config";
 
@@ -5,5 +6,13 @@ export default defineConfig({
   plugins: [hardhatToolboxMochaEthers],
   solidity: {
     version: "0.8.28",
+  },
+  networks: {
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: process.env.ALCHEMY_API_URL ?? "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
   },
 });
